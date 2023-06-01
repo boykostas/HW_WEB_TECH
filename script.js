75ddc2c96bade785c63334ec0948083bf394479b
Stanislav <boykostas@gmail.com>
2023-06-01 13:41:09 +0300

    добавил несколько ссылок и домашнее задание

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
-
 function app() {
     x = parseInt(document.getElementById("number").value);
+    document.getElementById("number").value = "";
     result = x ** 2;
     if (x==0) {
         result = "Введите число отличное от 0!";
@@ -10,4 +10,11 @@ function app() {
     }
     document.getElementById("res").innerText = result;
 }
+
+function fname() {
+    firstname = document.getElementById("fname").value;
+    document.getElementById("fname").value = "";
+    document.getElementById("fname_out").innerText = "Привет, дорогой друг, " + firstname + "!";
+}
+
 
