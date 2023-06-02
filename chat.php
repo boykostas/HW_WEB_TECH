7173179e7342efb0d69364762edcdf1733cb0cdc
Stanislav <boykostas@gmail.com>
2023-06-02 16:25:51 +0300

    Добавил файлы для чата

--- chat.php
+++ chat.php
@@ -1,46 +1,22 @@
 <?php 
+$file = fopen('chat.txt', 'a+');
 
+if (isset($_GET['message'])) {
+    $message = strip_tags($_GET['message']);
+    fputs($file, $message . "\n");
+    echo "ok";
+    die();
+}
 
-echo file_get_contents('chat.txt');
-?>
+$message = [];
 
-<!DOCTYPE html>
-<html lang="en">
+while (!feof($file)) {
+    $message[] = fgets($file);
+}
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>HW WEB TECH</title>
-    <link rel="stylesheet" href="style.css">
+$messages = array_reverse($messages);
 
-</head>
-
-<body>
-    <div class="conteiner">
-        <main class="content">
-            <header class="header">
-                <h1 class="header_top">Страница "Знакомство с веб-технологиями"</h1>
-            </header>
-            <section class="section_content">
-                <nav>
-                    <a href="index.html">Главная</a>
-                    <a href="name.html">Приветствие</a>
-                    <a href="chat.php">Чат</a>
-                </nav>
-            
-
-
-
-
-            </section>
-        </main>
-        
-        <footer class="footer">
-            <h3 class="footer_text">2023 год</h3>
-        </footer>
-    </div>
-    <script src="chat.js"></script>
-</body>
-
-</html>
+foreach ($message as $key => $str) {
+    echo $str;
+    if ($key >= 10) break;
+}
