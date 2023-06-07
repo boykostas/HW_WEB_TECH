373db1ed2aca2d5a53fb0fb6aee14d800c81df12
Stanislav <boykostas@gmail.com>
2023-06-07 15:27:59 +0300

    Homework #4

--- job.php
+++ job.php
@@ -12,7 +12,8 @@ $job = [
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+    <title>Job</title>
 </head>
 <body>
 <div class="w3-container w3-card w3-white w3-margin-bottom">
@@ -54,5 +55,5 @@ $job = [
     </div>
 </div>
 </body>
-</html><?php
+</html>
 
